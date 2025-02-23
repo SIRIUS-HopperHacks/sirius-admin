49df0317378b2b2ecaa6022227de4859321966d8
lighteko <dev.lighteko@gmail.com>
2025-02-23 09:41:24 -0500

    MOLA

--- src/components/pages/AdminLogInPage.tsx
+++ src/components/pages/AdminLogInPage.tsx
@@ -12,12 +12,6 @@ const AdminLogInPage: React.FC = () => {
     const formData = new FormData(e.currentTarget as HTMLFormElement);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    const confirmPassword = formData.get("confirmPassword") as string;
-
-    if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
 
     await api?.login(email, password);
     navigate("/homepage");
@@ -65,24 +59,6 @@ const AdminLogInPage: React.FC = () => {
           label="Password"
           type="password"
           name="password"
-          placeholder="*******"
-          sx={{
-            "& label": { color: "#980000" },
-            "& label.Mui-focused": { color: "#980000" },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": { borderColor: "#980000" },
-              "&:hover fieldset": { borderColor: "#7a0000" },
-              "&.Mui-focused fieldset": { borderColor: "#980000" },
-            },
-            "& input": { color: "#980000" },
-          }}
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Confirm Password"
-          type="password"
-          name="confirmPassword"
           placeholder="*******"
           sx={{
             "& label": { color: "#980000" },
